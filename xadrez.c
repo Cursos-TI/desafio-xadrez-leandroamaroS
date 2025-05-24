0fff1df2b1953c34a56358caa6f52a61067d97fc
leandroamaroS <leandroamarodesouza02@gmail.com>
2025-05-24 00:39:26 +0000

    commit xadrez nivel intermediario.

--- xadrez.c
+++ xadrez.c
@@ -9,19 +9,44 @@ while (torre <= 5)
 {
     printf(" A PEÇA TORRE MOVEU PARA A DIREITA. \n");
  torre++;
+
+ 
 }
+printf("\n");
+
 
 do
 {
  printf(" A PEÇA BISPO MOVEU PARA CIMA, DIREITA. \n");
  bispo++;
-} while (bispo <= 6);
+} while ( bispo <= 6);
+printf("\n");
 
 
-for (int rainha = 3; rainha <= 10; rainha++)
+for (int rainha = 1; rainha <= 8; rainha++)
 {
    printf(" A PEÇA RAINHA MOVEU PARA A ESQUERDA. \n");
 }
+
+printf("\n");
+
+
+for (int cavalo = 1; cavalo <= 2 ; cavalo++){
+
+   printf(" A PEÇA CAVALO MOVEU PARA BAIXO. \n");
+   while (cavalo == 2 )
+
+   {
+     printf(" A PEÇA CAVALO MOVEU PARA ESQUERDA. \n");
+
+     cavalo++;
+
+   }
+  
+}
+printf("\n");
+
+
 
 return 0;
 
